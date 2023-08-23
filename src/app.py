6ccffc4adce714a6c8460cb49493d23fe16f5791
Michael Lin <linmichael02@gmail.com>
2023-08-23 01:34:10 -0400

    Added another helper

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from nacl.signing import SigningKey
-from nacl.encoding import Base64Encoder
+from nacl.signing import VerifyKey
+from nacl.encoding import Base64Encoder, HexEncoder
+from nacl.hash import sha512
 from steamid import SteamID
+# To give to client
 encode_key = lambda signing_key: signing_key.encode(Base64Encoder).decode('utf-8')
+# To store in DB
+fingerprint = lambda verify_key: sha512(verify_key.encode(), encoder=HexEncoder).decode('utf-8')
 
 app = Flask(__name__)
 
